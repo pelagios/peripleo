a5c6d32b916a55277df64fe4c9189baf9f1ce8d6
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-22 16:45:10 +0200

    'Top places' bar chart on dataset view

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,20 +1,18 @@
-window.Heatmap = function(elId, dataURL) {
+window.Heatmap = function(mapId, places) {
   var awmcLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
     attribution: 'Tiles and Data &copy; 2013 <a href="http://www.awmc.unc.edu" target="_blank">AWMC</a> ' +
                  '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'});
   
-  var map = new L.Map(elId, {
+  var map = new L.Map(mapId, {
     center: new L.LatLng(41.893588, 12.488022),
     zoom: 3,
     layers: [awmcLayer]
   });
 
-  $.getJSON(dataURL, function(data) {
-    var latlngs = [];
-    $.each(data.items, function(idx, place) {
-      if (place.centroid_lat && place.centroid_lng)
-        latlngs.push([place.centroid_lat, place.centroid_lng]);
-    });    
-    L.heatLayer(latlngs, { max: 1.5, maxZoom: 0, radius: 5, blur: 6 }).addTo(map);
-  });
+  var latlngs = [];
+  $.each(places, function(idx, place) {
+    if (place.centroid_lat && place.centroid_lng)
+      latlngs.push([place.centroid_lat, place.centroid_lng]);
+  });    
+  L.heatLayer(latlngs, { max: 1.5, maxZoom: 0, radius: 5, blur: 6 }).addTo(map);
 };
