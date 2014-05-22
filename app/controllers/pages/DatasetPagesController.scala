521872daec70b4b8963d5c1022fc9c6dcac6f0f8
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-22 13:07:44 +0200

    Number formatting for HTML pages

--- app/controllers/pages/DatasetPagesController.scala
+++ app/controllers/pages/DatasetPagesController.scala
@@ -8,10 +8,14 @@ object DatasetPagesController extends Controller {
 
   def getDataset(id: String) = DBAction { implicit session =>
     val dataset = Datasets.findById(id)
-    if (dataset.isDefined)
-      Ok(views.html.dataset(dataset.get))
-    else
+    if (dataset.isDefined) {
+      val things = AnnotatedThings.countByDataset(dataset.get.id)
+      val places = Places.countPlacesInDataset(dataset.get.id)
+      val annotations = Annotations.countByDataset(dataset.get.id)
+      Ok(views.html.dataset(dataset.get, things, places, annotations))
+    } else {
       NotFound // TODO create decent 'not found' page
+    }
   }
   
 }
