ed216695e3e78aafc55a2581ba6dc1211e13707f
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-22 13:25:58 +0200

    Integrated heatmap with dataset view

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,14 +1,12 @@
-function loadHeatmap(dataURL) {
+window.Heatmap = function(elId, dataURL) {
   var awmcLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
     attribution: 'Tiles and Data &copy; 2013 <a href="http://www.awmc.unc.edu" target="_blank">AWMC</a> ' +
-                 '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'
-  });
+                 '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'});
   
-  var map = new L.Map('map', {
+  var map = new L.Map(elId, {
     center: new L.LatLng(41.893588, 12.488022),
-    zoom: 5,
-    layers: [awmcLayer],
-    minZoom: 3
+    zoom: 3,
+    layers: [awmcLayer]
   });
 
   $.getJSON(dataURL, function(data) {
@@ -19,4 +17,4 @@ function loadHeatmap(dataURL) {
     });    
     L.heatLayer(latlngs, { max: 1.5, maxZoom: 0, radius: 5, blur: 6 }).addTo(map);
   });
-}
+};
