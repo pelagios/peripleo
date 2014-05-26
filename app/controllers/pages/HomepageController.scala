4430e84fa139fc16f5c16f106370325d4e87c98a
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-26 21:27:40 +0200

    Minor tweaks to 'Home' view

--- app/controllers/pages/HomepageController.scala
+++ app/controllers/pages/HomepageController.scala
@@ -7,14 +7,21 @@ import play.api.Logger
 
 object HomepageController extends Controller {
   
+  private val QUERY = "query"
+  
   def index() = Action {
-    Ok(views.html.home())
+    Ok(views.html.home(1000, 1000, 1000))
   }
   
-  def search(query: String) = DBAction { implicit session =>
-    val results = Global.index.search(query, 50)
-    results.items.foreach(result => Logger.info(result.title))
-    Ok("")
+  def search() = DBAction { implicit session =>
+    val queryString = session.request.queryString.get(QUERY).flatMap(_.headOption)
+    if (queryString.isDefined) {
+      val results = Global.index.search(queryString.get, 50)
+      results.items.foreach(result => Logger.info(result.title))
+      Ok("")
+    } else {
+      BadRequest
+    }
   }
 
 }
