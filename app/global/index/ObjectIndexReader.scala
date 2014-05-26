e9dc0a694baf420eb543f59dff9664f473efef1c
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-26 20:19:45 +0200

    Lucene pagination

--- app/global/index/ObjectIndexReader.scala
+++ app/global/index/ObjectIndexReader.scala
@@ -1,23 +1,25 @@
 package global.index
 
+import models.Page
 import org.apache.lucene.index.DirectoryReader
-import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.{ IndexSearcher, TopScoreDocCollector }
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser
 import org.apache.lucene.util.Version
-import org.apache.lucene.search.TopScoreDocCollector
 
 trait ObjectIndexReader extends ObjectIndexBase {
   
-  def search(query: String, numHits: Int): Seq[IndexedObject] = {
+  def search(query: String, offset: Int = 0, limit: Int = 20): Page[IndexedObject] = {
     val searcher = searcherManager.acquire()
     try {
       val fields = Seq(ObjectIndex.FIELD_TITLE, ObjectIndex.FIELD_DESCRIPTION).toArray    
       val q = new MultiFieldQueryParser(Version.LUCENE_47, fields, analyzer).parse(query)
     
-      val collector = TopScoreDocCollector.create(numHits, true)
+      val collector = TopScoreDocCollector.create(offset + limit, true)
       searcher.search(q, collector)
     
-      collector.topDocs.scoreDocs.map(scoreDoc => new IndexedObject(searcher.doc(scoreDoc.doc)))
+      val total = collector.getTotalHits
+      val results = collector.topDocs(offset, limit).scoreDocs.map(scoreDoc => new IndexedObject(searcher.doc(scoreDoc.doc)))
+      Page(results.toSeq, offset, limit, total)
     } finally {
       searcherManager.release(searcher)
     } 
