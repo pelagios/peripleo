852fb27b440611b13e21b7a95fcf0495857f3066
Rainer Simon <rainer.simon@ait.ac.at>
2014-06-25 10:06:23 +0200

    Syncing with latest Scalagios changes

--- app/controllers/common/io/AbstractImporter.scala
+++ app/controllers/common/io/AbstractImporter.scala
@@ -2,24 +2,25 @@ package controllers.common.io
 
 import java.math.BigInteger
 import java.security.MessageDigest
-import org.openrdf.rio.RDFFormat
-import org.openrdf.rio.UnsupportedRDFormatException
+import org.pelagios.Scalagios
 
 abstract class AbstractImporter {
   
-  private val MD5 = "MD5"
+  private val SHA256 = "SHA-256"
+    
+  private val UTF8 = "UTF-8"
    
   /** Utility method that returns the RDF format corresponding to a particular file extension **/
-  protected def getFormat(filename: String): RDFFormat = filename match {
-    case f if f.endsWith("rdf") => RDFFormat.RDFXML
-    case f if f.endsWith("ttl") => RDFFormat.TURTLE
-    case f if f.endsWith("n3") => RDFFormat.N3
-    case _ => throw new UnsupportedRDFormatException("Format not supported")
+  protected def getFormat(filename: String): String = filename match {
+    case f if f.endsWith("rdf") => Scalagios.RDFXML
+    case f if f.endsWith("ttl") => Scalagios.TURTLE
+    case f if f.endsWith("n3") => Scalagios.N3
+    case _ => throw new IllegalArgumentException("Format not supported")
   }
   
   /** Utility method that produces a SHA256 hash from a string **/
   protected def sha256(str: String): String = {
-    val md = MessageDigest.getInstance("SHA-256").digest(str.getBytes("UTF-8"))
+    val md = MessageDigest.getInstance(SHA256).digest(str.getBytes(UTF8))
     new BigInteger(1, md).toString(16)
   }
 
