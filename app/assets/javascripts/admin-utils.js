ca64b0539994728af098fa9cc6e726264d7da351
Rainer Simon <rainer.simon@ait.ac.at>
2014-07-07 15:56:07 +0200

    A quick hack in the Admin GUI to enable data upload to subsets

--- app/assets/javascripts/admin-utils.js
+++ app/assets/javascripts/admin-utils.js
@@ -3,13 +3,13 @@ var util = util || {};
 /** Enables the file upload buttons **/
 util.enableUploads = function() {
   // Wire CSS-styled upload button to hidden file input 
-  $('.upload').click(function(e) {
+  $(document).on('click', '.upload', function(e) {
     var inputId = e.target.getAttribute('data-input');
     $('#' + inputId).click();  
   });
 
   // Set the hidden inputs to auto-submit
-  $('input:file').change(function(e) {
+  $(document).on('change', 'input:file', function(e) {
     e.target.parentNode.submit();  
   });
 }
