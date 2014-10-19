a1e8585f1a3244a236c6f0e4c17e28ea5750009c
Rainer Simon <rainer.simon@ait.ac.at>
2014-10-19 11:08:40 +0200

    Minor edits

--- app/index/IndexedObject.scala
+++ app/index/IndexedObject.scala
@@ -62,7 +62,7 @@ object IndexedObject {
     doc.add(new StringField(IndexFields.DATASET, thing.dataset, Field.Store.NO))
     doc.add(new TextField(IndexFields.TITLE, thing.title, Field.Store.YES))
     doc.add(new StringField(IndexFields.OBJECT_TYPE, IndexedObjectTypes.ANNOTATED_THING.toString, Field.Store.YES))
-    doc.add(new FacetField(IndexFields.OBJECT_TYPE, IndexedObjectTypes.ANNOTATED_THING.toString))
+    doc.add(new FacetField(IndexFields.OBJECT_TYPE, IndexedObjectTypes.ANNOTATED_THING.toString)) // Just add multiple strings for a path
     
     // Temporal bounds
     thing.temporalBoundsStart.map(d => doc.add(new IntField(IndexFields.DATE_FROM, d, Field.Store.YES)))
