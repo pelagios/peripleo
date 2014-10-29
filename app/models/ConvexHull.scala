f0a7b90599381e1bcd1370167e3ae2dcf2698253
Rainer Simon <rainer.simon@ait.ac.at>
2014-10-29 20:06:22 +0100

    Finished ConvexHull class

--- app/models/ConvexHull.scala
+++ app/models/ConvexHull.scala
@@ -1,24 +1,41 @@
 package models
 
-import play.api.db.slick.Config.driver.simple._
 import com.vividsolutions.jts.geom.Geometry
+import com.vividsolutions.jts.io.{ WKTReader, WKTWriter }
+import com.vividsolutions.jts.algorithm.{ ConvexHull => JTSConvexHull }
 import index.places.IndexedPlace
+import org.geotools.geometry.jts.JTSFactoryFinder
+import play.api.db.slick.Config.driver.simple._
+import scala.collection.JavaConverters._
 
-case class ConvexHull(geometry: Geometry) 
+case class ConvexHull(geometry: Geometry) {
+  
+  override lazy val toString = new WKTWriter().write(geometry)
+  
+}
 
 object ConvexHull {
   
   /** DB mapper function **/
   implicit val convexHullMapper = MappedColumnType.base[ConvexHull, String](
-    { hull => hull.toString },
-    { hull => ConvexHull.fromWKT(hull) })
+    { convexHull => convexHull.toString },
+    { convexHull => ConvexHull.fromWKT(convexHull) })
   
-  def fromIndexedPlaces(places: Seq[IndexedPlace]): ConvexHull = {
-    null
+  def compute(geometries: Seq[Geometry]): Option[ConvexHull] = {
+    if (geometries.size > 0) {
+      val factory = JTSFactoryFinder.getGeometryFactory()
+      val mergedGeometry = factory.buildGeometry(geometries.asJava).union
+      val cvGeometry = new JTSConvexHull(mergedGeometry).getConvexHull()
+      Some(ConvexHull(cvGeometry))
+    } else {
+      None
+    }
   }
+    
+  def fromIndexedPlaces(places: Seq[IndexedPlace]): Option[ConvexHull] =
+    compute(places.flatMap(_.geometry))
   
-  def fromWKT(wkt: String): ConvexHull = {
-    null
-  }
+  def fromWKT(wkt: String): ConvexHull =
+    ConvexHull(new WKTReader().read(wkt))
   
 }
