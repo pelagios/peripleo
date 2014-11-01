a632404a279715ac980f34e69f6522edcd989120
Rainer Simon <rainer.simon@ait.ac.at>
2014-11-01 22:18:18 +0100

    Various edits

--- app/global/housekeeping/AcessLogArchiver.scala
+++ app/global/housekeeping/AcessLogArchiver.scala
@@ -20,9 +20,7 @@ import scala.concurrent.duration.Duration
 object AccessLogArchiver {
   
   private val SEPARATOR = ";"
-    
-  val dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss")
-    
+        
   private lazy val LOG_ARCHIVE_DIR = {
     val dir = new File(Play.current.configuration.getString("api.log.archival.folder").getOrElse("logs"))
     if (!dir.exists)
@@ -30,6 +28,18 @@ object AccessLogArchiver {
     dir
   }
   
+  private lazy val ARCHIVAL_DAY = 
+    Play.current.configuration.getString("api.log.archival.day").getOrElse("1").trim().toInt
+    
+  private lazy val ARCHIVAL_TIME = {
+    val time = Play.current.configuration.getString("api.log.archival.time").getOrElse("2:00").split(":")
+    if (time.size != 2)
+      throw new IllegalArgumentException("Configuration contains invalid log archival time setting")
+    time
+  }
+  
+  private val dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss")
+
   /** Shorthand **/
   private def setTime(c: Calendar, hour: Int, minute: Int) = {
     c.set(Calendar.HOUR_OF_DAY, hour)
@@ -39,18 +49,13 @@ object AccessLogArchiver {
   }
 
   /** Delay to initial archive run - defaults to 1st of month, 2am **/
-  private val delay = {
-    val startDay = Play.current.configuration.getString("api.log.archival.day").getOrElse("1").trim().toInt
-    val startTime = Play.current.configuration.getString("api.log.archival.time").getOrElse("2:00").split(":")
-    if (startTime.size != 2)
-      throw new IllegalArgumentException("Configuration contains invalid log archival time setting")
-    
+  private def delayToNext = {
     // Create a calendar with today month & day + time from app.conf
     val now = new Date();
     val c = Calendar.getInstance()
     c.setTime(now)
-    c.set(Calendar.DAY_OF_MONTH, startDay)
-    setTime(c, startTime(0).toInt, startTime(1).toInt)
+    c.set(Calendar.DAY_OF_MONTH, ARCHIVAL_DAY)
+    setTime(c, ARCHIVAL_TIME(0).toInt, ARCHIVAL_TIME(1).toInt)
     
     // Are we already past that time this month? Set next month.
     if (c.getTimeInMillis < now.getTime)
@@ -117,37 +122,39 @@ object AccessLogArchiver {
     // Remove the original CSV
     csvFile.finalize()
   }
-  
-  def start() = {    
-    Akka.system.scheduler.scheduleOnce(delay) {
-      
-      DB.withSession { implicit s: Session =>
-        val startTime = System.currentTimeMillis
-        
-        // Retrieve all log records older than one month
-        val dateLimit = Calendar.getInstance()
-        dateLimit.setTime(new Date())
-        setTime(dateLimit, 0, 0) // Last night, 0am
-        dateLimit.add(Calendar.MONTH, -1) // One month before
-        
-        val dateLimitMillis = dateLimit.getTimeInMillis
-        Logger.info("Archiving access logs older than " + new Date(dateLimitMillis))
 
-        val toArchive = AccessLog.findAllBefore(dateLimitMillis)
-        if (toArchive.size > 0) {
-          Logger.info(toArchive.size + " log records")
-          
-          // Write to file
-          writeToZip(dateLimit, toArchive)
+  private def doArchiving(): Unit = {
+    DB.withSession { implicit s: Session =>
+      val startTime = System.currentTimeMillis
         
-          // Drop from database
-          AccessLog.deleteAllBefore(dateLimitMillis)
-          Logger.info("Archiving complete - took " + (System.currentTimeMillis - startTime) + " ms")
-        } else {
-          Logger.info("Nothing to archive - skipping")
-        }
+      // Retrieve all log records older than one month
+      val dateLimit = Calendar.getInstance()
+      dateLimit.setTime(new Date())
+      setTime(dateLimit, 0, 0) // Last night, 0am
+      dateLimit.add(Calendar.MONTH, -1) // One month before
+        
+      val dateLimitMillis = dateLimit.getTimeInMillis
+      Logger.info("Archiving access logs older than " + new Date(dateLimitMillis))
+
+      val toArchive = AccessLog.findAllBefore(dateLimitMillis)
+      if (toArchive.size > 0) {
+        Logger.info(toArchive.size + " log records")
+          
+        // Write to file
+        writeToZip(dateLimit, toArchive)
+        
+        // Drop from database
+        AccessLog.deleteAllBefore(dateLimitMillis)
+        Logger.info("Archiving complete - took " + (System.currentTimeMillis - startTime) + " ms")
+      } else {
+        Logger.info("Nothing to archive - skipping")
       }
     }
+    
+    Akka.system.scheduler.scheduleOnce(delayToNext) { doArchiving() }
   }
+  
+  def start() =
+    Akka.system.scheduler.scheduleOnce(delayToNext) { doArchiving() }
 
 }
