d99dc66971e9b6563aaa694e1614851aadcbdcfa
Rainer Simon <rainer.simon@ait.ac.at>
2015-09-29 13:10:48 +0200

    Log output re startup gazetteer ingest

--- app/global/Global.scala
+++ app/global/Global.scala
@@ -18,23 +18,24 @@ import play.api.db.slick._
 import scala.slick.jdbc.meta.MTable
 
 object Global extends GlobalSettings {
-  
+
   private val GAZETTER_DATA_DIR = "gazetteer"
-    
+
   private var accessLogArchiver: Option[Cancellable] = None
-       
+
   lazy val index = {
     val idx = Index.open("index")
-    
+
     if (idx.numPlaceNetworks == 0) {
       Logger.info("Building place index...")
-      
+
       // Grab the list of gazetteers from the application properties file
-      def getPropertyAsList(name: String): Seq[String] = 
+      def getPropertyAsList(name: String): Seq[String] =
         Play.current.configuration.getString(name).map(_.split(",").toSeq).getOrElse(Seq.empty[String]).map(_.trim)
-      
+
       val gazetteers = getPropertyAsList("peripleo.gazetteer.names").zip(getPropertyAsList("peripleo.gazetteer.files"))
-      
+      Logger.info("Loading gazetteers: " + gazetteers.map(_.toString).mkString(", "))
+
       // Helper function to insert a single dump file into the index (returning number of total places, distinct places and URI prefixes)
       def insertDumpfile(sourceGazetteer: String, file: File): (Int, Int, Seq[String]) = {
         val (is, filename)  = if (file.getName.endsWith(".gz"))
@@ -44,12 +45,12 @@ object Global extends GlobalSettings {
 
         idx.addPlaceStream(is, filename, sourceGazetteer)
       }
-      
+
       // Build the place index
       DB.withSession { implicit session: Session =>
         gazetteers.foreach { case (name, dump) => {
           Logger.info("Loading gazetteer " + name  + ": " + dump)
-        
+
           val file = new File(GAZETTER_DATA_DIR, dump)
           val (totalPlaces, distinctPlaces, uriPrefixes) =
             if (file.isDirectory) {
@@ -62,23 +63,23 @@ object Global extends GlobalSettings {
             } else {
               insertDumpfile(name, file)
             }
-          
+
           idx.refresh()
-          
+
           // Insert gazetteer meta in to DB
           Gazetteers.insert(Gazetteer(name, totalPlaces), uriPrefixes)
         }}
-       
+
         // Apply gazetteer patches
         val patches = Play.current.configuration.getString("peripleo.gazetteer.patches")
           .map(_.split(",").toSeq).getOrElse(Seq.empty[String]).map(_.trim)
-       
+
         // Patching strategy is to REPLACE geometries, but APPEND names
         val patchConfig = PatchConfig()
           .geometry(PatchStrategy.REPLACE)
           .names(PatchStrategy.APPEND)
           .propagate(true) // Patches should propagate to linke places, too
-      
+
         patches.foreach(patch => {
           Logger.info("Applying gazetteer patch file " + patch)
           idx.applyPatch(new File(GAZETTER_DATA_DIR, patch), patchConfig)
@@ -86,7 +87,7 @@ object Global extends GlobalSettings {
           Logger.info("Done.")
         })
       }
-      
+
       // Rebuild the suggestion index
       idx.suggester.build()
     }
@@ -101,17 +102,17 @@ object Global extends GlobalSettings {
         Logger.info("DB table 'datasets' does not exist - creating")
         Datasets.create
       }
-            
+
       if (MTable.getTables("annotated_things").list.isEmpty) {
         Logger.info("DB table 'annotated_things' does not exist - creating")
         AnnotatedThings.create
       }
-      
+
       if (MTable.getTables("images").list.isEmpty) {
         Logger.info("DB table 'images' does not exist - creating")
         Images.create
-      }  
-       
+      }
+
       if (MTable.getTables("annotations").list.isEmpty) {
         Logger.info("DB table 'annotations' does not exist - creating")
         Annotations.create
@@ -121,42 +122,42 @@ object Global extends GlobalSettings {
         Logger.info("DB table 'gazetteers' does not exist - creating")
         Gazetteers.create
       }
-      
+
       if (MTable.getTables("place_to_dataset_associations").list.isEmpty || MTable.getTables("place_to_thing_associations").list.isEmpty) {
         Logger.info("DB tables 'place_to_dataset_associations' and 'place_to_thing_associations' do not exist - creating")
         Associations.create
       }
-      
+
       if (MTable.getTables("tags").list.isEmpty) {
         Logger.info("DB table 'tags' does not exist - creating")
         Tags.create
-      }  
+      }
 
       if (MTable.getTables("adjacency_places").list.isEmpty) {
         Logger.info("DB table 'adjacency_place' does not exist - creating")
         PlaceAdjacencys.create
-      }  
-      
+      }
+
       /*
       if (MTable.getTables("master_heatmap").list.isEmpty) {
         Logger.info("DB table 'master_heatmap' does not exist - creating")
         MasterHeatmap.create
-      }    
+      }
       */
-      
+
       if (MTable.getTables("access_log").list.isEmpty) {
         Logger.info("DB table 'access_log' does not exit - creating")
         AccessLog.create
-      }  
+      }
     }
-    
+
     // Start log archiving demon
     accessLogArchiver = Some(AccessLogArchiver.start())
-  }  
-  
+  }
+
   override def onStop(app: Application): Unit = {
     index.close()
-    
+
     if (accessLogArchiver.isDefined) {
       Logger.info("Shutting down log archival background actor")
       accessLogArchiver.get.cancel
