c23edbe277c206c43585e739e5c4aac40f7ecfde
Rainer Simon <rainer.simon@ait.ac.at>
2015-10-29 12:34:15 +0100

    Zip import for CSV/TEI batches

--- app/controllers/admin/DatasetAdminController.scala
+++ app/controllers/admin/DatasetAdminController.scala
@@ -1,20 +1,25 @@
 package controllers.admin
 
+import collection.JavaConverters._
 import global.Global
+import ingest._
+import ingest.harvest.DataHarvester
+import java.io.File
+import java.util.zip.{ ZipEntry, ZipFile }
 import models.Associations
 import models.core._
 import play.api.db.slick._
 import play.api.Logger
 import play.api.libs.json.Json
 import scala.io.Source
-import ingest._
-import ingest.harvest.DataHarvester
 
 object DatasetAdminController extends BaseUploadController with Secured {
   
   private val UTF8 = "UTF-8"
   private val CSV = "csv"
   private val TEIXML = "tei.xml"
+  private val ZIP = "zip"
+  
   private val TMP_DIR = System.getProperty("java.io.tmpdir")
   
   def index = adminAction { username => implicit requestWithSession =>
@@ -58,7 +63,8 @@ object DatasetAdminController extends BaseUploadController with Secured {
       
       val subsetsRecursive = id +: Datasets.listSubsetsRecursive(id)
     
-      // Purge from database
+      // Purge from databaseimport java.io.File
+
       Logger.info("Dropping annotations")
       Annotations.deleteForDatasets(subsetsRecursive)
       
@@ -94,6 +100,8 @@ object DatasetAdminController extends BaseUploadController with Secured {
           CSVImporter.importRecogitoCSV(Source.fromFile(filepart.ref.file, UTF8), dataset.get)
         else if (filepart.filename.endsWith(TEIXML)) 
           TEIImporter.importTEI(Source.fromFile(filepart.ref.file, UTF8), dataset.get)
+        else if (filepart.filename.endsWith(ZIP)) 
+          importZip(filepart.ref.file, dataset.get)
         else
           PelagiosOAImporter.importPelagiosAnnotations(filepart.ref, filepart.filename, dataset.get)
         Redirect(routes.DatasetAdminController.index).flashing("success" ->
@@ -107,6 +115,18 @@ object DatasetAdminController extends BaseUploadController with Secured {
   def rebuildAutoSuggestionIndex = adminAction { username => implicit requestWithSession => 
     Global.index.suggester.build()
     Status(200)
+  }
+  
+  private def importZip(file: File, dataset: Dataset)(implicit s: Session) = {
+    val zipFile = new ZipFile(file)
+    val entries = zipFile.entries.asScala.toSeq.filter(!_.getName.startsWith("__MACOSX"))
+    entries.foreach(entry => {
+      Logger.info("Importing " + entry.getName)
+      if (entry.getName.endsWith(TEIXML))
+        TEIImporter.importTEI(Source.fromInputStream(zipFile.getInputStream(entry), UTF8), dataset)
+      else if (entry.getName.endsWith(CSV))
+        CSVImporter.importRecogitoCSV(Source.fromInputStream(zipFile.getInputStream(entry), UTF8), dataset)
+    })
   }
   
 }
